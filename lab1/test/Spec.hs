1921090f66f4e70d18bdd8e73163d354fcc0f33e
mikhail <rusmih98@yandex.ru>
2021-03-07 14:47:41 +0700

    изменен вывод в тестах

--- lab1/test/Spec.hs
+++ lab1/test/Spec.hs
@@ -1,11 +1,19 @@
 import Lib(intersect)
 main :: IO ()
 main = do
+  print "[]"
   print (intersect [])
+  print [[1,2,3]]
   print (intersect [[1,2,3]])
+  print [[1,2,3], [4,5,6]]
   print (intersect [[1,2,3], [4,5,6]])
+  print [[1,2,3], [2,3,4]]
   print (intersect [[1,2,3], [2,3,4]])
+  print [[1,2,3], [2,3,4], [3..5]]
   print (intersect [[1,2,3], [2,3,4], [3..5]])
+  print [[1,2,3], [2,3,4], [3..5], [3,7,2]]
   print (intersect [[1,2,3], [2,3,4], [3..5], [3,7,2]])
+  print [[1,2,3], [2,3,4], [3..5], [3,7,2], [9]]
   print (intersect [[1,2,3], [2,3,4], [3..5], [3,7,2], [9]])
+  print [[1,2,3], [2,3,4], [3..5], [3,7,2], []]
   print (intersect [[1,2,3], [2,3,4], [3..5], [3,7,2], []])
